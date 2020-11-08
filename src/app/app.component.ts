152fb4eb9718393ad8977f16aae54b9440cc775a
Sleepybaby666 <kirin.ccb@gmail.com>
2020-11-07 18:06:04 -0600

    sat.spec.ts still throwing error but everything else running proper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,12 @@ import { Satellite } from './satellite';
 })
 export class AppComponent {
   title = 'orbit-report';
-  sourceList: Satellite[];
+  sourceList: Satellite[] = [];
+  displayList: Satellite[] =[];
 
 
 constructor() {
+  this.displayList = []
   this.sourceList = [];
   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
@@ -20,13 +22,30 @@ constructor() {
 
         let fetchedSatellites = data.satellites;
         for (let i = 0; i < fetchedSatellites.length; i++){
-            let satellite = new Satellite(fetchedSatellites[i].name,fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational)
+            let satellite = new Satellite(
+              fetchedSatellites[i].name,
+              fetchedSatellites[i].type, 
+              fetchedSatellites[i].launchDate, 
+              fetchedSatellites[i].orbitType, 
+              fetchedSatellites[i].operational)
             this.sourceList.push(satellite);
           }
         
-
+         this.displayList = this.sourceList.slice(0);
      }.bind(this));
   }.bind(this));
-
 }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    this.displayList = matchingSatellites;
+ }
+
 };
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,12 +4,16 @@ export class Satellite {
   type: string;
   operational: boolean;
   launchDate: string;
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
-      this.name = name;
-      this.orbitType = orbitType;
-      this.type = type;
-      this.operational = operational;
-      this.launchDate = launchDate;
-    }
 
+  constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+      this.name = name;
+      this.type = type;
+      this.launchDate = launchDate;
+      this.orbitType = orbitType;
+      this.operational = operational;
+  }
+
+  shouldShowWarning(): boolean {
+      return this.type.toLowerCase() === "space debris";
+  }
 }
